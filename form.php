f54f2ff67056a15c4686bbbe553c277cb36a269c
VaKa31 <151768590+VaKa31@users.noreply.github.com>
2023-12-26 22:54:23 +0500

    Create form.php

---
+++ form.php
@@ -0,0 +1,12 @@
+ <?php
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $password2 = $_POST['password2'];
+            if (isset($username)){
+                echo "<h1 class='hello_block' style='    
+                text-align:center;
+                background-color:white;
+                color:black;
+                '>HEllO  $username</h1>";
+                }
+ ?>
